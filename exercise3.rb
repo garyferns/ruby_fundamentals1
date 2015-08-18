2725f545158c1bd36cc45cc05db78603a3b60581
Chris Fernandes <chrisfernandes@Chriss-MacBook-Air.local>
2015-08-18 16:36:11 -0400

    Exercise 3 complete

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,16 @@
 
+puts "What's your name?
+"
 
-puts "What's your name?"
+name = gets.strip
+
+puts "
+Hi #{name}!"
+
+puts " 
+I hope you don't mind me asking, but how old are you?"
+
+a = gets.to_i
+
+puts"
+Cool! That means your born around #{2015-a}. That was a good year."
