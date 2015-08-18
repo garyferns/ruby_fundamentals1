2ba18e87592ad9e6a1bfb372d7c849c5cadb12a7
Chris Fernandes <chrisfernandes@Chriss-MacBook-Air.local>
2015-08-18 17:28:22 -0400

    Exercise 4

---
+++ exercise4.rb
@@ -0,0 +1,32 @@
+
+count = 1.0
+
+
+while count<=100
+
+if count % 15 == 0
+
+	puts "Bitmaker"
+
+	elsif count % 3 == 0
+
+		puts "Bit" 	
+
+			elsif count % 5 == 0
+
+			puts "maker" 
+
+			else
+
+				puts "#{count}"	
+
+end 
+
+
+count += 1.0
+
+end
+
+
+
+
