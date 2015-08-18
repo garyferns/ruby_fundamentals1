c7b3aac925eab42ae6719c5b53e25a8559986f9e
Chris Fernandes <chrisfernandes@Chriss-MacBook-Air.local>
2015-08-18 16:00:09 -0400

    Exercise 2updated

--- exercise2.rb
+++ exercise2.rb
@@ -4,5 +4,40 @@
 a = 55
 b = 0.15
 
-puts "At a rate of 15%, a good tip for a $55 meal is #{a*b} "
+
+puts " Question 1
+
+At a rate of 15%, a good tip for a $55 meal is #{a*b} "
+
+#Question 2
+
+puts "
+
+Question 2
+
+If you specify that a string is equivalent to a number, then you can add an integer to a string. See the code for the calculation! :)"
+puts "
+For example, 7 + a, where a is 55, is equal to #{7 + a}"
+
+
+
+#Question 3
+
+puts "
+
+Question 3
+
+The solution of 45628 multiplied by 7839 is #{45628*7839}"
+
+
+
+#Question 4
+
+
+puts" 
+
+Question 4
+
+
+#{(true && false) || (false && true) || !(false && false)}"
 
